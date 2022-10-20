26af0fd172d06014a5b40fb364c1c4c53f07e2f4
choidy180 <choidy180@naver.com>
2022-10-20 11:05:48 +0900

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+	<footer>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+<script>
+	let a = 1;
+	let b = 2;
+</script>
+<div>
+	<input type="number" bind:value={a}>
+	<input type="number" bind:value={b}>
+	<p>{a} + {b} = {a + b}</p>	
+</div>
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100vw;
+		height: 50vh;
+		overflow: none;
+	}
+</style>
